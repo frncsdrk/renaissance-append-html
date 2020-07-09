895e50e71eda031d946b6de735372afdba961f06
frncsdrk <frncsdrk@protonmail.com>
2020-07-09 22:37:02 +0200

    Test adapter with renaissance

--- test/Adapter.spec.js
+++ test/Adapter.spec.js
@@ -1,13 +1,19 @@
 const test = require("tape");
+const { r } = require("renaissance")
 const Adapter = require("../src/Adapter");
 
-test("Adapter can be instantiated", (t) => {
+test("Adapter can be instantiated", t => {
   t.ok(new Adapter());
   t.end();
 });
 
-test("Adapter has appendHTML method", (t) => {
+test("Adapter has appendHTML method", t => {
   const adapter = new Adapter();
   t.ok(typeof adapter.appendHTML === "function");
   t.end();
 });
+
+test("Adapter can be registered with renaissance", t => {
+  t.ok(r.registerAdapter("appendHTML", Adapter))
+  t.end()
+})
